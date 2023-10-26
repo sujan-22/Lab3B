de3610a5ab8c4ea44292dc78947016be79e6f2cc
sujan-22 <sujanrokad44@gmail.com>
2023-10-26 19:06:08 -0400

    comments added

--- Lab3B/Form1.Designer.cs
+++ Lab3B/Form1.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.listBox2 = new System.Windows.Forms.ListBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.listBox3 = new System.Windows.Forms.ListBox();
             this.addServices = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -50,6 +50,7 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(31, 28);
@@ -59,44 +60,10 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Select a Hairdresser:";
             // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.listBox1);
-            this.groupBox2.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(276, 28);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(249, 226);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Select a Service:";
-            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.listBox2);
-            this.groupBox3.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox3.Location = new System.Drawing.Point(534, 28);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(248, 226);
-            this.groupBox3.TabIndex = 1;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Charged Items:";
-            // 
-            // groupBox4
-            // 
-            this.groupBox4.Controls.Add(this.listBox3);
-            this.groupBox4.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox4.Location = new System.Drawing.Point(791, 28);
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(162, 226);
-            this.groupBox4.TabIndex = 2;
-            this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "Price:";
-            // 
             // comboBox1
             // 
             this.comboBox1.AllowDrop = true;
-            this.comboBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Menu;
             this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.comboBox1.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
@@ -113,9 +80,22 @@
             this.comboBox1.Text = "Jane Samley";
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(276, 28);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(249, 226);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select a Service:";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
             // listBox1
             // 
-            this.listBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.listBox1.BackColor = System.Drawing.SystemColors.Menu;
             this.listBox1.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 23;
@@ -132,9 +112,21 @@
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.groupBox3.Controls.Add(this.listBox2);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(534, 28);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(248, 226);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Charged Items:";
+            // 
             // listBox2
             // 
-            this.listBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.listBox2.BackColor = System.Drawing.SystemColors.Menu;
             this.listBox2.Enabled = false;
             this.listBox2.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox2.FormattingEnabled = true;
@@ -145,9 +137,21 @@
             this.listBox2.TabIndex = 1;
             this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
+            // groupBox4
+            // 
+            this.groupBox4.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.groupBox4.Controls.Add(this.listBox3);
+            this.groupBox4.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox4.Location = new System.Drawing.Point(791, 28);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(162, 226);
+            this.groupBox4.TabIndex = 2;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Price:";
+            // 
             // listBox3
             // 
-            this.listBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.listBox3.BackColor = System.Drawing.SystemColors.Menu;
             this.listBox3.Enabled = false;
             this.listBox3.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox3.FormattingEnabled = true;
@@ -159,9 +163,12 @@
             // 
             // addServices
             // 
-            this.addServices.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.addServices.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addServices.AutoEllipsis = true;
+            this.addServices.BackColor = System.Drawing.Color.DarkCyan;
+            this.addServices.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.addServices.FlatAppearance.BorderSize = 2;
             this.addServices.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addServices.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.addServices.Location = new System.Drawing.Point(31, 339);
             this.addServices.Name = "addServices";
             this.addServices.Size = new System.Drawing.Size(239, 52);
@@ -172,9 +179,12 @@
             // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.AutoEllipsis = true;
+            this.button2.BackColor = System.Drawing.Color.DarkCyan;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button2.FlatAppearance.BorderSize = 2;
             this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button2.Location = new System.Drawing.Point(280, 339);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(239, 52);
@@ -185,9 +195,12 @@
             // 
             // button3
             // 
-            this.button3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.AutoEllipsis = true;
+            this.button3.BackColor = System.Drawing.Color.DarkCyan;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button3.FlatAppearance.BorderSize = 2;
             this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button3.Location = new System.Drawing.Point(534, 339);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(239, 52);
@@ -198,15 +211,19 @@
             // 
             // button4
             // 
-            this.button4.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.AutoEllipsis = true;
+            this.button4.BackColor = System.Drawing.Color.DarkCyan;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button4.FlatAppearance.BorderSize = 2;
             this.button4.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button4.Location = new System.Drawing.Point(791, 339);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(162, 52);
             this.button4.TabIndex = 6;
             this.button4.Text = "Exit";
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // label1
             // 
@@ -232,7 +249,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.ClientSize = new System.Drawing.Size(965, 450);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -244,8 +261,10 @@
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Form1";
             this.Text = "Perfect Cut Hair Salon";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
--- Lab3B/Form1.cs
+++ Lab3B/Form1.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+Class:  Form1.cs
+Author: Sujan Rokad
+Date:   October 26, 2023
+
+Purpose: This class represents the main form of the Hair Salon Pricing Calculator application. It provides a graphical user interface (GUI) for users to select a hairdresser, one or more services, and calculate the total price for the salon services. Users can also reset their selections and exit the application.
+
+Authorship statement: I, Sujan Rokad, 000882948 certify that this material is my original work.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,24 +27,33 @@ namespace Lab3B
             InitializeComponent();
         }
 
-        private void groupBox2_Enter(object sender, EventArgs e)
-        {
+        // Group of methods related to event handlers and form initialization
 
+
+        /// <summary>
+        /// Handles the form's Load event and initializes button states.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            addServices.Enabled = false; // Disable Add Service button
+            button2.Enabled = false; // Disable Calculate button
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
         {
-
+            Application.Exit();
         }
 
-        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
+        /// <summary>
+        /// Handles the "Reset" button's click event and resets selections and controls.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button3_Click(object sender, EventArgs e)
         {
-            // Clear all the ListBox items
+            // Clear all ListBox items
             listBox2.Items.Clear();
             listBox3.Items.Clear();
 
@@ -44,27 +63,48 @@ namespace Lab3B
             // Clear the TextBox
             textBox1.Clear();
 
-            comboBox1 .Items.Clear();
+            // Reset the selected index of the ComboBox
+            comboBox1.SelectedIndex = 0;
+
+            // Reset the selected indices of the ListBox
             listBox1.SelectedIndex = -1;
+
+            // Disable "Add Service" button
+            addServices.Enabled = false;
+
+            // Disable "Calculate" button
+            button2.Enabled = false;
         }
 
+
+
         private bool hairdresserAdded = false; // Add this field to track if the hairdresser has been added
+        /// <summary>
+        /// Handles the "Add Service" button's click event, calculates prices, and displays results.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
+            // Get the selected hairdresser from the ComboBox
             string selectedHairdresser = comboBox1.SelectedItem.ToString();
 
+            // Create a list to store selected services from the ListBox
             List<string> services = new List<string>();
+
             foreach (var items in listBox1.SelectedItems)
             {
                 services.Add(items.ToString());
             }
 
-
+            // Initialize the total price
             decimal totalPrice = 0;
 
+            // Extract the first name of the selected hairdresser
             string[] selectedHairdresserParts = selectedHairdresser.Split(' ');
             string firstName = selectedHairdresserParts[0]; // Extract the first name
 
+            // Calculate the base price based on the selected hairdresser
             if (firstName == "Jane")
             {
                 totalPrice = 30;
@@ -86,8 +126,10 @@ namespace Lab3B
                 totalPrice = 55;
             }
 
-
+            // Initialize the service price
             decimal servicePrice = 0;
+
+            // Calculate the price for each selected service
             foreach (var service in services)
             {
                 switch (service)
@@ -121,27 +163,64 @@ namespace Lab3B
                 hairdresserAdded = true; // Set the flag to true to indicate that it has been added
             }
 
-
+            // Display the selected services in ListBox2
             foreach (var service in services)
             {
                 listBox2.Items.Add($"{service}");
             }
 
+            // Display the service price in ListBox3
             listBox3.Items.Add($"${servicePrice}");
 
         }
 
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
 
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Handles the ListBox1's SelectedIndexChanged event. It enables "Add Service" and "Calculate" buttons based on selections.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Check if at least one item is selected in listBox1
+            if (listBox1.SelectedItems.Count > 0)
+            {
+                addServices.Enabled = true; // Enables Add Service button
+                button2.Enabled = true; // Enables Calculate button
+            }
+
+        }
+
+        /// <summary>
+        /// Handles the "Calculate" button's click event, calculates the total price based on the items in ListBox3, and displays it in textBox1.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button2_Click(object sender, EventArgs e)
         {
+            // Initialize the total price
             decimal totalPrice = 0;
 
+            // Iterate through items in ListBox3 to calculate the total price
             foreach (var item in listBox3.Items)
             {
+                // Check if the item is a string and starts with a '$' character
                 if (item is string itemString && itemString.StartsWith("$"))
                 {
                     // Extract the price value (remove the '$' character and parse as decimal)
@@ -165,5 +244,11 @@ namespace Lab3B
         {
 
         }
+
+        
+
+        
+
+        
     }
 }
